7c0b5e3a073965e36363d325da7b71e3d1c7a080
unknown <jcarrillo@SJ-STA-001-L349.clarocr.americamovil.ca1>
2021-03-18 10:55:16 -0600

    Complete

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+// import { Link } from "react-router-dom";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="https://www.linkedin.com/in/jocarrill0/">José Carrillo Bermúdez</a>
+			<a href="https://www.linkedin.com/in/jocarrill0/" target="_blank" rel="noopener noreferrer">
+				José Carrillo Bermúdez
+			</a>
 		</p>
 	</footer>
 );
